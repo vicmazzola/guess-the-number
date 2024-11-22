2b74a4baa55ff62c6a1034fd4600d1106c21e6a4
Victor Mazzola <victormazzola2019@gmail.com>
2024-11-22 08:08:37 -0300

    test game

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const inputNumber = document.getElementById("input-number");
 const buttonNumber = document.getElementById("button");
+const resetButton = document.getElementById("reset-button");
 const resultParagraph = document.getElementById("result");
 
-const result = Math.floor((Math.random() * 100) + 1);
-console.log("Number to guess: ", result);
+let result = Math.floor((Math.random() * 100) + 1);
 
 let attemptsLeft = 5;
 
@@ -11,18 +11,22 @@ buttonNumber.addEventListener("click", function (event) {
     event.preventDefault();
 
     const guess = parseInt(inputNumber.value);
+    inputNumber.value = "";
 
-    attemptsLeft --;
+    attemptsLeft--;
 
     if (attemptsLeft === 0 && guess !== result) {
         resultParagraph.innerText = `Game Over! The correct number was: ${result}`;
         buttonNumber.disabled = true;
+        resetButton.style.display = "block";
         return;
     }
 
 
     if (guess === result) {
         resultParagraph.innerText = `Congratulations! You guessed the correct number: ${result}`;
+        buttonNumber.disabled = true;
+        resetButton.style.display = "block";
     } else if (guess > result) {
         resultParagraph.innerText = `Too high! Try again \nAttempts left: ${attemptsLeft}`;
     } else if (guess < result) {
@@ -30,6 +34,13 @@ buttonNumber.addEventListener("click", function (event) {
     }
 
 })
-
+resetButton.addEventListener("click", function () {
+    attemptsLeft = 5;
+    result = Math.floor((Math.random() * 100) + 1);
+    buttonNumber.disabled = false;
+    resetButton.style.display = "none";
+    resultParagraph.innerText = "Here's the result!";
+    inputNumber.value = "";
+});
 
 
